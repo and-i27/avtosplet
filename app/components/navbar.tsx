7f5d677b7d7b4c8a8547e96b99fbba94e3c31602
Andi <andrej.rezonicnik@gmail.com>
2025-11-11 08:20:15 +0100

    popravek se ene tiskarske napake

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -29,7 +29,7 @@ const session = await auth();
                             </button>
                         </form>
 
-                        <Link href={`/user/${session?.id}`}>
+                        <Link href={`/user/${session?.user?.id}`}>
                             <span>{session?.user?.name}</span>
                         </Link>
                     </>
